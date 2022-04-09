89703c60cd4576ada34a61490a672b00572cf8ee
DESKTOP-3L721M6\USER <wjdtjq1121@gmail.com>
2022-04-09 09:10:12 +0900

    implement combination code practice

---
+++ CombinationTest_practice.java
@@ -0,0 +1,27 @@
+package comb;
+import java.util.Arrays;
+
+public class CombinationTest_practice {
+
+	static int N=3,R=2; 
+	static int[] input;
+	static int[] numbers;
+	
+	public static void main(String[] args) {
+
+		input = new int[]{1,4,7};
+		numbers = new int[R];
+	
+		combination(0,0);
+	}
+	
+	private static void combination(int cnt,int start) {
+
+
+	}
+	
+	
+	
+	
+
+}
