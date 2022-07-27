cbffd7234db79dfd712e24b6c43900d6f2e9382e
DESKTOP-3L721M6\USER <wjdtjq1121@gmail.com>
2022-07-27 18:40:55 +0900

    solved programmers 짝지어제거하기

---
+++ 프로그래머스_짝지어제거하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c: s.toCharArray()) {
+            
+            if(stack.isEmpty()) {
+                stack.push(c);
+            } else if(stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+             
+        }
+
+        
+        
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
